ece4a3ba653a15c2a8469924115b0e58fee8a128
Kyuhan (Leo) Lee <leoleeidc@gmail.com>
2024-12-15 15:12:49 +0900

    add title

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <cstdint>
 #include <format>
 #include <fstream>
@@ -12,7 +13,10 @@ int main(int argc, char **argv)
 {
     std::string pathString = argc > 1 ? argv[1] : "result.md";
     std::filesystem::path filepath = pathString;
-    std::filesystem::create_directories(filepath.parent_path());
+    if (filepath.has_parent_path())
+    {
+        std::filesystem::create_directories(filepath.parent_path());
+    }
 
     std::vector<std::unique_ptr<ILottery>> lotteries;
     lotteries.emplace_back(std::make_unique<Lotto645>());
@@ -20,6 +24,12 @@ int main(int argc, char **argv)
 
     std::ofstream ofs;
     ofs.open(filepath, std::ofstream::out | std::ofstream::trunc);
+    {
+        // const std::chrono::year_month_day ymd{std::chrono::floor<std::chrono::days>(std::chrono::system_clock::now())};
+        // ofs << std::format("---\ntitle: Weekly Numbuers{}\n---\n\n", ymd);
+        ofs << "---\ntitle: Weekly Numbers {{ date | date('dddd, MMMM YYYY - Wo') }}\n---\n\n";
+    }
+
     for (auto &&lottery : lotteries)
     {
         ofs << std::format("# {}\n", lottery->GetName());
