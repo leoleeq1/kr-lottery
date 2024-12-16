5a92f9deb4dcc6b593035c5f2a5f9af3900f96ad
Kyuhan (Leo) Lee <leoleeidc@gmail.com>
2024-12-16 11:12:05 +0900

    skip annuity

--- src/main.cpp
+++ src/main.cpp
@@ -20,13 +20,11 @@ int main(int argc, char **argv)
 
     std::vector<std::unique_ptr<ILottery>> lotteries;
     lotteries.emplace_back(std::make_unique<Lotto645>());
-    lotteries.emplace_back(std::make_unique<AnnuityLottery720Plus>());
+    // lotteries.emplace_back(std::make_unique<AnnuityLottery720Plus>());
 
     std::ofstream ofs;
     ofs.open(filepath, std::ofstream::out | std::ofstream::trunc);
     {
-        // const std::chrono::year_month_day ymd{std::chrono::floor<std::chrono::days>(std::chrono::system_clock::now())};
-        // ofs << std::format("---\ntitle: Weekly Numbuers{}\n---\n\n", ymd);
         ofs << "---\ntitle: Weekly Numbers {{ date | date('MMMM YYYY - Wo') }}\n---\n\n";
     }
 
